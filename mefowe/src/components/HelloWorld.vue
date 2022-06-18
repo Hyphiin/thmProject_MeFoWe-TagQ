6e352c43970312dc36d813c74eb80eb3fb10e3c2
Felix Wegener <felix.wegener@iem.thm.de>
2022-06-18 11:10:27 +0200

    wip

--- mefowe/src/components/HelloWorld.vue
+++ mefowe/src/components/HelloWorld.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="hello">
     <editor-content :editor="editor" />
+    <button @click="addRect">Add Rect</button>
+    <v-stage
+      ref="stage"
+      :config="configKonva"
+      @dragstart="handleDragstart"
+      @dragend="handleDragend"
+    >
+      <v-layer ref="layer">
+        <v-rect
+          v-for="item in list"
+          :key="item.id"
+          :config="{
+            x: 100,
+            y: 100,
+            rotation: 85,
+            id: '1',
+            fill: 'red',
+            shadowBlur: 10,
+            width: 100,
+            height: 100,
+            draggable: true,
+          }"
+        />
+      </v-layer>
+    </v-stage>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { StarterKit } from "@tiptap/starter-kit/src/starter-kit";
 import { useEditor, EditorContent } from "@tiptap/vue-3/src";
+import { Rect } from "konva/lib/shapes/Rect";
 
 export default defineComponent({
   name: "Task",
@@ -24,7 +50,53 @@ export default defineComponent({
       extensions: [StarterKit],
     });
 
-    return { editor };
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    const list = ref<Rect[]>([]);
+    const dragItemId = ref<string>();
+
+    const configKonva = ref({
+      width: width,
+      height: height,
+    });
+
+    const handleDragstart = (e: DragEvent) => {
+      if (e.target != null && e.target instanceof HTMLElement) {
+        // save drag element:
+        dragItemId.value = e.target.id;
+        // move current element to the top:
+        const item = list.value.find((i) => i.id() === dragItemId.value);
+        const index = list.value.indexOf(item as Rect);
+        list.value.splice(index, 1);
+        list.value.push(item as Rect);
+      }
+    };
+    const handleDragend = () => {
+      dragItemId.value = "";
+    };
+
+    const addRect = () => {
+      list.value.push(
+        new Rect({
+          id: Math.round(Math.random() * 10000).toString(),
+          x: 100,
+          y: 100,
+          rotation: 75,
+          draggable: true,
+        })
+      );
+    };
+
+    return {
+      editor,
+      list,
+      dragItemId,
+      configKonva,
+      handleDragstart,
+      handleDragend,
+      addRect,
+    };
   },
 });
 </script>
